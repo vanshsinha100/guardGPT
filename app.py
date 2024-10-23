3fadf3cd77d947665bebc570693a2cad9f0fa35f
Vansh Sinha <vanshsinha100@gmail.com>
2024-10-24 00:32:27 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    text = [token for token in text if token.isalnum()]
+    text = [token for token in text if token not in list(stopwords.words('english'))]
+    text = [token for token in text if token not in string.punctuation]
+    ps = PorterStemmer()
+    text = [ps.stem(token)for token in text]
+    text = ' '.join(text)
+    return text
+
+#tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("AI Jailbreak Prompt Classifier")
+
+input_prompt = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_text_ai = transform_text(input_prompt)
+    # 2. vectorize
+    # 3. predict
+    result = model.predict([transformed_text_ai])[0]
+    # 4. Display
+    if result == 1:
+        st.header("Jailbreak")
+    else:
+        st.header(f"Benign")
